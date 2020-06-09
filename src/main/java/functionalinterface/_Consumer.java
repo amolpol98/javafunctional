fd3a47fab821b67fa64df3e0d21cdbf945c0732f
Amol Pol <amol.pol@here.com>
2020-06-10 00:06:16 +0530

    BiConsumer functional interface intro and example; Bi means these functional interfaces take 2 arguments

--- src/main/java/functionalinterface/_Consumer.java
+++ src/main/java/functionalinterface/_Consumer.java
@@ -1,22 +1,33 @@
 package functionalinterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
         Customer amol = new Customer("Amol", "123123");
-
         // Normal java function
         greetCustomer(amol);
+        greetCustomerV2(amol, false);
 
         // Consumer Functional interface
         greetCustomerConsumer.accept(amol);
+
+        greetCustomerConsumerV2.accept(amol, false);
     }
 
-    static Consumer<Customer> greetCustomerConsumer = customer ->  System.out.println("hello " + customer.customerName + ", your phone number - " + customer.customerPhoneNumber);
+    static Consumer<Customer> greetCustomerConsumer =
+            customer ->  System.out.println("hello " + customer.customerName + ", your phone number - " + customer.customerPhoneNumber);
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerV2 =
+            (customer, showPhoneNumber) ->  System.out.println("hello " + customer.customerName + ", your phone number - " + (showPhoneNumber ? customer.customerPhoneNumber : "******"));
 
     static void greetCustomer(Customer customer) {
         System.out.println("hello " + customer.customerName + ", your phone number - " + customer.customerPhoneNumber);
+    }
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("hello " + customer.customerName + ", your phone number - " + (showPhoneNumber ? customer.customerPhoneNumber : "******"));
     }
 
     static class Customer {
