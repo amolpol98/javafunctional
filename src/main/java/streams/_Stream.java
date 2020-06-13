e41a29fd0b8a00d329ce494831a7bad8f7a42a6a
Amol Pol <amol.pol@here.com>
2020-06-13 20:34:27 +0530

    Introduction to java streams; making use of map, collect, foreach which internally relies on functional interfaces

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,52 @@
+package streams;
+
+import imperative.Main;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
+
+public class _Stream {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("Amol", MALE),
+                new Person("Ashish", MALE),
+                new Person("Aditya", MALE),
+                new Person("Vikram", MALE),
+                new Person("Vanita", FEMALE)
+        );
+
+        people.stream()
+                .map(person -> person.gender)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE;
+    }
+}
